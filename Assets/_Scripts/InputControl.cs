c861228b6ec4e5fbc98c0e3ac90b61e24bd89ac8
Lucas <lucas.cosson@metropolia.com>
2014-01-25 10:02:41 +0200

    First commit

---
+++ Assets/_Scripts/InputControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputControl : MonoBehaviour
+{
+    #region MEMBERS
+    private bool b_cubeClicked = false;
+    #endregion
+    #region UNITY_METHODS
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag == "Cube")
+                {
+                    hit.rigidbody.isKinematic = true;
+                    Vector3 pos = hit.transform.position;
+                    pos.y += 2f;
+                    hit.transform.position = pos;
+                    b_cubeClicked = true;
+                }
+            }
+        }
+        if (b_cubeClicked == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+
+            }
+        }
+    }
+    #endregion
+}
---
+++ Assets/_Scripts/Manager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : GameManager
+{
+
+    #region MEMBERS
+    private float m_timer = 120;
+    private Rect m_timerRect;
+    #endregion
+
+    #region UNITY_METHODS
+    void Start () 
+    {
+        e_state = State.StartMenu;
+        float size = Screen.width / 10f;
+        float halfWidth = Screen.width / 2f;
+        m_timerRect = new Rect(halfWidth - size/ 2f, 0, size, size);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (e_state != State.Running) return;
+
+        m_timer -= Time.deltaTime;
+    }
+    void OnGUI() 
+    {
+        int minutes = (int)(m_timer / 60);
+        int seconds = (int)(m_timer % 60);
+        GUI.Box(m_timerRect,minutes.ToString() + ":"+seconds.ToString("00"));
+    }
+    #endregion
+
+    #region METHODS
+
+    #endregion
+}
+
+
