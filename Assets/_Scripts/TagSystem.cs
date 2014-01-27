ee27f2b16d7f90cd775f8d8ba1039110e186e5c1
Lucas <lucas.cosson@metropolia.com>
2014-01-27 10:41:28 +0200

    Projector

---
+++ Assets/_Scripts/TagSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TagSystem : MonoBehaviour {
+
+    public BuildingType buildingType;
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+    public void SwaptTag(BuildingType newType) 
+    {
+        buildingType = newType;
+    }
+    void OnGUI() 
+    {
+    
+    }
+}
+public enum BuildingType
+{ 
+    House, Environment
+}
