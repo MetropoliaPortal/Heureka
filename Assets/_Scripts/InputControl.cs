8600e706711ed4896e7178054c6dafe4a0443b5f
Lucas Cosson <lucas.cosson@metropolia.fi>
2014-01-27 13:03:08 +0200

    Newest

--- Assets/_Scripts/InputControl.cs
+++ Assets/_Scripts/InputControl.cs
@@ -5,17 +5,19 @@ public class InputControl : MonoBehaviour
 {
     #region MEMBERS
     private bool b_cubeClicked = false;
+	private Transform m_selectedCube;
     #endregion
     #region UNITY_METHODS
     // Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        if (Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0) && !b_cubeClicked)
         { 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -23,21 +25,38 @@ public class InputControl : MonoBehaviour
             {
                 if (hit.collider.tag == "Cube")
                 {
-                    hit.rigidbody.isKinematic = true;
-                    Vector3 pos = hit.transform.position;
+					GameObject obj = hit.collider.gameObject;
+					m_selectedCube = obj.transform.parent;
+                    Vector3 pos = m_selectedCube.transform.position;
                     pos.y += 2f;
-                    hit.transform.position = pos;
+                    m_selectedCube.transform.position = pos;
                     b_cubeClicked = true;
+
+					m_selectedCube.rigidbody.isKinematic = true;
+					obj.layer = LayerMask.NameToLayer("Ignore Raycast");
+					b_cubeClicked = true;
                 }
             }
         }
+		else if(Input.GetMouseButtonDown(0) && b_cubeClicked)
+		{
+			b_cubeClicked = false;
+			m_selectedCube.rigidbody.isKinematic = false;
+			m_selectedCube.gameObject.layer = LayerMask.NameToLayer("Default");
+			m_selectedCube.transform.Find ("Cube").gameObject.layer =  LayerMask.NameToLayer("Default");
+		}
         if (b_cubeClicked == true)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-
+				if(hit.collider.tag == "Floor" || hit.collider.tag == "Cube")
+				{
+					Vector3 vec = hit.point;
+					vec.y +=2f;
+					m_selectedCube.position = vec;
+				}
             }
         }
     }
---
+++ Assets/_Scripts/SecBoxScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SecBoxScript : MonoBehaviour {
+
+	public Transform m_boxTransform;
+	private Transform m_transform;
+	void Start () 
+	{
+		m_transform = GetComponent<Transform>();
+		Vector3 pos = m_boxTransform.position;
+		pos.z -= 50f;
+		m_transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 pos = m_boxTransform.position;
+		pos.z -= 50f;
+		m_transform.position = pos;
+	}
+}
