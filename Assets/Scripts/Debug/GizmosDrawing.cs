8fa43a885ff3fbdb5dd7dd07c931353370062c20
Lucas <lucas.cosson@metropolia.com>
2014-04-26 17:11:18 +0300

    Fixed the state issue (Timer.cs to be removed)

---
+++ Assets/Scripts/Debug/GizmosDrawing.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GizmosDrawing : MonoBehaviour {
+	public Color color;
+	public float size = 1f;
+	void OnDrawGizmosSelected() {
+		Gizmos.color = color;
+		Gizmos.DrawSphere(transform.position, size);
+	}
+}
