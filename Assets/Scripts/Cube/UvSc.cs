032c6496701760876826c924ac25e791db44f031
Ville Sillanpaa <sillanpaa.ville@hotmail.com>
2014-05-07 16:18:47 +0300

    fixed uvmaps

--- Assets/Scripts/Cube/UvSc.cs
+++ Assets/Scripts/Cube/UvSc.cs
@@ -10,7 +10,7 @@ using System.Collections;
 public class UvSc : MonoBehaviour {
 
 	// DEBUGGING PURPOSE
-	/*
+
 	void Start () {
 		Mesh theMesh;
 		theMesh = GetComponent<MeshFilter>().mesh;
@@ -23,42 +23,48 @@ public class UvSc : MonoBehaviour {
 		// set UV co-ordinates
 		
 		// Top
+		theUVs[4] = new Vector2(0.0f, 1.0f);
+		theUVs[5] = new Vector2(0.33f, 1.0f);
 		theUVs[8] = new Vector2(0.0f, 0.0f);
 		theUVs[9] = new Vector2(0.33f, 0.0f);
-		theUVs[4] = new Vector2(0.0f, 1f);
-		theUVs[5] = new Vector2(0.33f, 1f);
 		
-		// Back
+		// Front
 		theUVs[1] = new Vector2(0.33f, 0.0f);
 		theUVs[0] = new Vector2(0.66f, 0.0f);
 		theUVs[3] = new Vector2(0.33f, 1f);
 		theUVs[2] = new Vector2(0.66f, 1f);
-		// Right
-		theUVs[20] = new Vector2(0.66f, 0f);
-		theUVs[22] = new Vector2(1f, 0.00f);
-		theUVs[23] = new Vector2(0.66f, 1f);
-		theUVs[21] = new Vector2(1f, 1f);
+
+		// Back
+		theUVs[6] = new Vector2(0.33f, 0.0f);
+		theUVs[7] = new Vector2(0.66f, 0.0f);
+		theUVs[10] = new Vector2(0.33f, 1.0f);
+		theUVs[11] = new Vector2(0.66f, 1.0f);
+
 		// Left
-		theUVs[7] = new Vector2(0.33f, 0.0f);
-		theUVs[6] = new Vector2(0.66f, 0.0f);
-		theUVs[11] = new Vector2(0.33f, 1f);
-		theUVs[10] = new Vector2(0.66f, 1f);
-		// Front
-		theUVs[18] = new Vector2(0.66f, 0f);
-		theUVs[16] = new Vector2(1f, 0.00f);
-		theUVs[17] = new Vector2(0.66f, 1f);
-		theUVs[19] = new Vector2(1f, 1f);
+		theUVs[19] = new Vector2(0.66f, 1.0f);
+		theUVs[17] = new Vector2(1.0f, 1.0f);
+		theUVs[16] = new Vector2(0.66f, 0.0f);
+		theUVs[18] = new Vector2(1.0f, 0.0f);
+
+		// Right
+		theUVs[23] = new Vector2(0.66f, 1.0f);
+		theUVs[21] = new Vector2(1.0f, 1.0f);
+		theUVs[20] = new Vector2(0.66f, 0.0f);
+		theUVs[22] = new Vector2(1.0f, 0.0f);
+
 		// Assign the mesh its new UVs
 		theMesh.uv = theUVs;
 	}
-	*/
+
 	/// <summary>
 	/// Initialize the specified cubeObject.
 	/// The method is called from the StartScript.cs
 	/// The Cube object to be taken care is passed as parameter
 	/// </summary>
 	/// <param name="cubeObject">Cube object.</param>
-	public void Initialize (GameObject cubeObject) {
+	public void Initialize (GameObject cubeObject) 
+	{
+
         Mesh theMesh;
 		theMesh = cubeObject.GetComponent<MeshFilter>().mesh;
 		
