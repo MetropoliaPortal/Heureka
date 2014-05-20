46661799b7bcb4643991cde007ae61e6e538854e
Ville Sillanpaa <sillanpaa.ville@hotmail.com>
2014-05-20 13:22:21 +0300

    missing files

---
+++ Assets/Scripts/Cube/CubeDebug.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeDebug : MonoBehaviour 
+{
+	void Start () 
+	{
+		TagInfo info = GetComponent<TagInfo>();
+		info.BuildingType = BuildingType.Leisure;
+		GameObject.Find("GameManager").GetComponent<TagManager>().tagInfos.Add( info );
+	}
+}
---
+++ Assets/Scripts/Global/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Helpers  
+{
+	public static bool ComparePreviousValues(Queue<int> previousValues, int key, int ComparePreviousAmount)
+	{
+		if (previousValues.Count < ComparePreviousAmount) 
+		{
+			previousValues.Enqueue (key);
+		} 
+		else 
+		{
+			previousValues.Dequeue();
+		}
+		foreach (int i in previousValues) 
+		{
+			if( i != key)
+				return false;
+		}
+		return true;
+	}	
+}
